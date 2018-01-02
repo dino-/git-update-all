3f86d9c92b35b075a8353d83bcea15a1596978fa
Dino Morelli <dino@ui3.info>
2018-01-01 21:26:47 -0500

    Finished switching this over to update git repos
    Now also exits a failure code if ANY git repo failed to update properly.
    This will help alert an operator of any problems.

--- darcs-pull-all.hs
+++ darcs-pull-all.hs
@@ -4,17 +4,17 @@
    This is intended to be run regularly from a cron job or
    similar. Something like this is what I use:
 
-      00 04 * * *  sh -c '/path/to/git-pull-all.hs /var/lib/git >> /path/to/git-pull-all.log' || echo "ERROR exit code: $?"
+      00 04 * * *  sh -c '/path/to/git-update-all.hs /var/lib/git >> /path/to/git-update-all.log' || echo "ERROR exit code: $?"
 -}
 
-import Control.Monad ( filterM )
+import Control.Monad ( forM )
 import Data.List ( isPrefixOf, sort )
 import Data.Time.Clock ( getCurrentTime )
 import Data.Time.Format ( defaultTimeLocale, formatTime )
 import Data.Time.LocalTime ( utcToLocalZonedTime )
 import System.Directory ( getDirectoryContents, setCurrentDirectory )
 import System.Environment ( getArgs, getProgName )
-import System.Exit ( ExitCode ( ExitSuccess ) )
+import System.Exit ( ExitCode ( ExitSuccess ), die, exitSuccess )
 import System.FilePath
 import System.IO ( BufferMode ( NoBuffering )
    , hSetBuffering, stdin, stdout, stderr )
@@ -40,16 +40,17 @@ main = do
    -- Turn them into absolute paths
    let allProjectsAbs = map (gitDir </>) allProjects
 
-   -- Only repos that have a remote set
-   --remoteProjects <- filterM hasRemote allProjectsAbs
-
-   -- Pull in each of them
-   --mapM_ pull remoteProjects
-
    -- Perform a `git remote update` in each of them
-   mapM_ update allProjectsAbs
+   results <- forM allProjectsAbs $ \path -> do
+      printf "Updating repository at %s\n" path
+      setCurrentDirectory path
+      system "git remote update"
 
    logM "Completed"
+
+   if (any (/= ExitSuccess) results)
+      then die "Failed to update one or more git repositories. Please check the log."
+      else exitSuccess
 
 
 parseArgs :: [String] -> IO FilePath
@@ -62,27 +63,7 @@ parseArgs _               = usage
 usage :: IO a
 usage = do
    pn <- getProgName
-   error $ printf "usage: %s DIR_CONTAINING_DARCS_REPOS" pn
-
-
-update :: FilePath -> IO Bool
-update path = do
-   setCurrentDirectory path
-   ok <$> system "git remote update"
-
-
-hasRemote :: FilePath -> IO Bool
-hasRemote path = do
-   setCurrentDirectory path
-   ok `fmap` system "darcs show repo | grep -q Remote"
-
-
-pull :: FilePath -> IO Bool
-pull path = do
-   setCurrentDirectory path
-
-   -- Could optionally add --verbose to see the patch descriptions
-   ok `fmap` system "darcs pull --all 2>&1"
+   error $ printf "usage: %s DIR_CONTAINING_GIT_REPOS" pn
 
 
 {- Get the current date/time as a string in the specified format
@@ -99,10 +80,3 @@ logM :: String -> IO ()
 logM msg = do
    tstamp <- dateFormat "%F %T"
    printf "%s> %s\n" tstamp msg
-
-
-{- Turn an exit code (say, from system) into a Bool
--}
-ok :: ExitCode -> Bool
-ok ExitSuccess = True
-ok _           = False
